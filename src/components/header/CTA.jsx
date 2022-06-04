cfa45273ede08e00a3668b44926e808c1695b2fd
CJ <cjbastien44@gmail.com>
2022-06-04 18:08:12 +0100

    First commit

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CV from "../../assets/cv.pdf";
+
+const CTA = () => {
+  return (
+    // <Fragment>
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
+    </div>
+    // </Fragment>
+  );
+};
+
+export default CTA;
